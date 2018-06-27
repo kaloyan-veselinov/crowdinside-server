01e5c34e7c0e18272e5ebaee34a335603ab4fa9b
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-06-27 17:33:22 +0200

    Step stateful entity javadoc

--- src/com/kaloyanveselinov/tracesgeneration/stepdetector/StepStateful.java
+++ src/com/kaloyanveselinov/tracesgeneration/stepdetector/StepStateful.java
@@ -1,7 +1,13 @@
+/**
+ * Stateful entity for the step detection FSM
+ *
+ * @author Kaloyan Veselinov
+ * @version 1.0
+ * @see <a href="http://www.statefulj.org/fsm/#define-your-stateful-entity"></a>
+ */
 package com.kaloyanveselinov.tracesgeneration.stepdetector;
 
 import org.statefulj.persistence.annotations.State;
-
 import java.sql.Timestamp;
 
 public class StepStateful {
@@ -10,14 +16,26 @@ public class StepStateful {
     private Timestamp startTime;
     private Timestamp endTime;
 
-    public Timestamp getStartTime() {
+    /**
+     * Gets the step start time
+     * @return the step start time
+     */
+    Timestamp getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(Timestamp startTime) {
+    /**
+     * Sets the step start time
+     * @param startTime the start time
+     */
+    void setStartTime(Timestamp startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Gets the step end time
+     * @return the step end time
+     */
     public Timestamp getEndTime() {
         return endTime;
     }
@@ -26,6 +44,10 @@ public class StepStateful {
         this.endTime = endTime;
     }
 
+    /**
+     * Gets the currents state of the stateful entity
+     * @return the current state
+     */
     public String getState() {
         return state;
     }
