ee716ce105e797277916e9d718cf01a6c94d5ee4
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-06-27 17:26:19 +0200

    StepFSM javadoc

--- src/com/kaloyanveselinov/tracesgeneration/stepdetector/StepFSM.java
+++ src/com/kaloyanveselinov/tracesgeneration/stepdetector/StepFSM.java
@@ -1,3 +1,14 @@
+/**
+ * The StepFSM class is used to detect a step as described in Uptime
+ *
+ * The class uses the Statefulj FSM library
+ *
+ * @author Kaloyan Veselinov
+ * @version 1.0
+ * @see <a href="http://ieeexplore.ieee.org/abstract/document/6214359/"></a>
+ * @see <a href="http://www.statefulj.org/"></a>
+ */
+
 package com.kaloyanveselinov.tracesgeneration.stepdetector;
 
 import com.kaloyanveselinov.datacollection.AggregatedReading;
@@ -15,13 +26,13 @@ import java.util.List;
 class StepFSM extends FSM<StepStateful> {
     private StepDetector detector;
 
-    // Thresholds
+    // Thresholds as defined in UPTIME
     private static final double THR = 10.4;
     private static final double POS_PEEK_THR = 11.6;
     private static final double NEG_PEEK_THR = 8.8;
     private static final double NEG_THR = 9.2;
 
-    // Events
+    // FSM transition events
     private static final String ltThr = "Input < Thr";
     private static final String htThr = "Input > Thr";
     private static final String htNegThr = "Input > Neg_Thr";
@@ -29,7 +40,7 @@ class StepFSM extends FSM<StepStateful> {
     private static final String ltNegPeekThr = "Input < Neg_Peek_Thr";
     private static final String htNegPeekThr = "Input > Neg_Peek_Thr";
 
-    // States
+    // FSM states
     private static final State<StepStateful> s0 = new StateImpl<>("S0");
     private static final State<StepStateful> s1 = new StateImpl<>("S1");
     private static final State<StepStateful> s2 = new StateImpl<>("S2");
@@ -38,21 +49,30 @@ class StepFSM extends FSM<StepStateful> {
     private static final State<StepStateful> s5 = new StateImpl<>("S5");
     private static final State<StepStateful> s6 = new StateImpl<>("S6", true);
 
-    // Actions
+    // FSM action when the beginning of a step is detected; sets the step start time
     private Action<StepStateful> startStep = (stepStateful, s, objects) -> {
         stepStateful.setStartTime((Timestamp) objects[0]);
     };
+    // FSM action when the end of a step is detected; sets the end time and calls a callback
     private Action<StepStateful> endStep = (stepStateful, s, objects) -> {
         stepStateful.setEndTime((Timestamp) objects[0]);
         detector.onStepDetected();
     };
 
+    /**
+     * Constructor for the StepFSM using the provided step detector (for the end step callback)
+     * @param detector the StepDetector
+     */
     StepFSM(StepDetector detector) {
         super("StepStateful FSM", new MemoryPersisterImpl<>(getStates(), s0));
         initTransitions();
         this.detector = detector;
     }
 
+    /**
+     * Initializes the list of possible states as per Statefulj's specifications
+     * @return the list of possible states
+     */
     private static List<State<StepStateful>> getStates() {
         List<State<StepStateful>> states = new LinkedList<>();
         states.add(s0);
@@ -65,7 +85,9 @@ class StepFSM extends FSM<StepStateful> {
         return states;
     }
 
-    // Transitions
+    /**
+     * Initializes the transitions from one state to another as per Statefulj's specifications
+     */
     private void initTransitions() {
         s0.addTransition(htThr, s1, startStep);
 
@@ -85,6 +107,12 @@ class StepFSM extends FSM<StepStateful> {
         s6.addTransition(ltThr, s0, null);
     }
 
+    /**
+     * Updates the state of the StepFSM on the arrival of a new reading
+     * @param stepStateful the associated stateful entity to update
+     * @param aggregatedReading the new reading
+     * @throws TooBusyException if an error has occurred in Statefulj
+     */
     void updateStateOnReading(StepStateful stepStateful, AggregatedReading aggregatedReading) throws TooBusyException {
         double accMagn = aggregatedReading.getAccelerationMagnitude();
         Timestamp timestamp = aggregatedReading.getTimestamp();
