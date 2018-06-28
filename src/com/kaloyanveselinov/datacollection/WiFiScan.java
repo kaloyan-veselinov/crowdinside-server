9d93c0a4e1ead258596556c59e41167dba5b5015
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-06-28 11:00:22 +0200

    Comments for WiFiScan

--- src/com/kaloyanveselinov/datacollection/WiFiScan.java
+++ src/com/kaloyanveselinov/datacollection/WiFiScan.java
@@ -7,28 +7,22 @@ import org.json.JSONObject;
 import java.sql.Timestamp;
 import java.util.LinkedList;
 
+/**
+ * Class for a WiFi scan produced by the app
+ *
+ * @author Kaloyan Veselinov
+ * @version 1.0
+ */
 public class WiFiScan {
-
-    public class WiFiRecord{
-        private String bssid;
-        private int level;
-        private int channel;
-
-        public WiFiRecord(JSONObject wiFiAPData){
-            try {
-                this.bssid = wiFiAPData.getString("bssid");
-                this.level = wiFiAPData.getInt("level");
-                this.channel = wiFiAPData.getInt("channel");
-            } catch (JSONException e){
-                e.printStackTrace();
-            }
-        }
-    }
-
     private LinkedList<WiFiRecord> wifiAccessPoints;
     private Timestamp timestamp;
 
-    public WiFiScan(JSONObject wifiScanData, Timestamp timestamp){
+    /**
+     * Parses the WiFi scan from the JSONObject
+     * @param wifiScanData the WiFi scan to parse
+     * @param timestamp the time of the reading
+     */
+    WiFiScan(JSONObject wifiScanData, Timestamp timestamp){
         this.timestamp = timestamp;
         wifiAccessPoints = new LinkedList<>();
         try{
@@ -48,6 +42,25 @@ public class WiFiScan {
 
     public Timestamp getTimestamp() {
         return timestamp;
+    }
+
+    /**
+     * Class for a single WiFi access point data in the scan
+     */
+    public class WiFiRecord{
+        private String bssid;
+        private int level;
+        private int channel;
+
+        WiFiRecord(JSONObject wiFiAPData){
+            try {
+                this.bssid = wiFiAPData.getString("bssid");
+                this.level = wiFiAPData.getInt("level");
+                this.channel = wiFiAPData.getInt("channel");
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
     }
 
 }
