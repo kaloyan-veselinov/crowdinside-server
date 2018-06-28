54dd542046ce206180bd7c2bcefe1deef106a2ce
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-06-28 10:42:58 +0200

    Comments for AggregatedReading

--- src/com/kaloyanveselinov/datacollection/AggregatedReading.java
+++ src/com/kaloyanveselinov/datacollection/AggregatedReading.java
@@ -17,22 +17,82 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Class representing a sample of aggregated data extracted from raw data
+ *
+ * Raw data from the app is averaged in order to group values from different sensors and to reduce the noise
+ *
+ * @author Kaloyan Veselinov
+ * @version 2.0
+ */
 public class AggregatedReading {
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
     private Timestamp timestamp;
-    private Timestamp startTimestamp;
+    private Timestamp startTimestamp; // used to specify the zero time value (first timestamp in the data set)
     private Stat acceleration;
     private Stat magnetometer;
     private Stat gyroscope;
 
+    /**
+     * Constructor for an empty AggregatedReading
+     * @param startTimestamp the reference time value used to set the 0 time
+     * @param timestamp the timestamp for the AggregatedReading
+     */
     AggregatedReading(Timestamp startTimestamp, Timestamp timestamp) {
         this.startTimestamp = startTimestamp;
         this.timestamp = timestamp;
     }
 
+    /**
+     * Prints a list of AggregatedReadings to a CSV file
+     * @param aggregatedReadings the list to export
+     * @param filename the name of the output file
+     * @param type the user's gait (ground truth for machine learning algorithms)
+     */
+    static void toCSV(List<AggregatedReading> aggregatedReadings, String filename, String type) {
+        BufferedWriter bufferedWriter;
+        try {
+            String[] params = {"acceleration", "magnetometer", "gyroscope"};
+            String[] HEADERS = buildHeaders(params);
+            bufferedWriter = new BufferedWriter(new FileWriter("aggregated" + filename));
+            CSVPrinter csvPrinter = new CSVPrinter(bufferedWriter, CSVFormat.EXCEL.withHeader(HEADERS));
+            for (AggregatedReading aggregatedReading : aggregatedReadings) {
+                aggregatedReading.printReadingToCSV(csvPrinter, type);
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints a single AggregatedReading to the provided csvPrinter stream
+     * @param csvPrinter the CSV printer to print to
+     * @param type the user's gait
+     * @throws IOException if an I/O exception has occured during the writing
+     */
+    private void printReadingToCSV(CSVPrinter csvPrinter, String type) throws IOException {
+        csvPrinter.printRecord(timestamp.getTime() - startTimestamp.getTime(),
+                acceleration.meanVector.getX(), acceleration.meanVector.getY(), acceleration.meanVector.getZ(),
+                acceleration.meanVector.getNorm(), acceleration.simpleRegression.getSlope(), acceleration.descriptiveStatistics.getVariance(),
+                acceleration.descriptiveStatistics.getKurtosis(), acceleration.descriptiveStatistics.getSkewness(),
+                acceleration.correlation[0], acceleration.correlation[1], acceleration.correlation[2],
+                magnetometer.meanVector.getX(), magnetometer.meanVector.getY(), magnetometer.meanVector.getZ(),
+                magnetometer.meanVector.getNorm(), magnetometer.simpleRegression.getSlope(), magnetometer.descriptiveStatistics.getVariance(),
+                magnetometer.descriptiveStatistics.getKurtosis(), magnetometer.descriptiveStatistics.getSkewness(),
+                magnetometer.correlation[0], magnetometer.correlation[1], magnetometer.correlation[2],
+                gyroscope.meanVector.getX(), gyroscope.meanVector.getY(), gyroscope.meanVector.getZ(),
+                gyroscope.meanVector.getNorm(), gyroscope.simpleRegression.getSlope(), gyroscope.descriptiveStatistics.getVariance(),
+                gyroscope.descriptiveStatistics.getKurtosis(), gyroscope.descriptiveStatistics.getSkewness(),
+                gyroscope.correlation[0], gyroscope.correlation[1], gyroscope.correlation[2], type);
+
+
+    }
+
+    /**
+     * Builds the headers of the CSV file
+     * @param params a string with all parameters (acceleration, magnetism, gyro, etc)
+     * @return a list containing the headers for the CSV file
+     */
     private static String[] buildHeaders(String[] params) {
         LinkedList<String> headers = new LinkedList<>();
         headers.add("timestamp");
@@ -53,55 +113,31 @@ public class AggregatedReading {
         return headers.toArray(new String[headers.size()]);
     }
 
-
+    /**
+     * Aggregates acceleration data until maxTime has been reached
+     * @param it an iterator over raw acceleration data
+     * @param maxTime the maximum timestamp for this AggregatedReading
+     */
     void setAcceleration(Iterator<InertialSensorRecord> it, long maxTime) {
         acceleration = new Stat(it, maxTime);
     }
 
+    /**
+     * Aggregates gyroscope data until maxTime has been reached
+     * @param it an iterator over raw gyroscope data
+     * @param maxTime the maximum timestamp for this AggregatedReading
+     */
     void setGyroscope(Iterator<InertialSensorRecord> it, long maxTime) {
         gyroscope = new Stat(it, maxTime);
     }
 
+    /**
+     * Aggregates magnetometer data until maxTime has been reached
+     * @param it an iterator over raw magnetometer data
+     * @param maxTime the maximum timestamp for this AggregatedReading
+     */
     void setMagnetometer(Iterator<InertialSensorRecord> it, long maxTime) {
         magnetometer = new Stat(it, maxTime);
-    }
-
-    private void printReadingToCSV(CSVPrinter csvPrinter, String type) throws IOException {
-        csvPrinter.printRecord(timestamp.getTime() - startTimestamp.getTime(),
-                acceleration.meanVector.getX(), acceleration.meanVector.getY(), acceleration.meanVector.getZ(),
-                acceleration.meanVector.getNorm(), acceleration.simpleRegression.getSlope(), acceleration.descriptiveStatistics.getVariance(),
-                acceleration.descriptiveStatistics.getKurtosis(), acceleration.descriptiveStatistics.getSkewness(),
-                acceleration.correlation[0], acceleration.correlation[1], acceleration.correlation[2],
-                magnetometer.meanVector.getX(), magnetometer.meanVector.getY(), magnetometer.meanVector.getZ(),
-                magnetometer.meanVector.getNorm(), magnetometer.simpleRegression.getSlope(), magnetometer.descriptiveStatistics.getVariance(),
-                magnetometer.descriptiveStatistics.getKurtosis(), magnetometer.descriptiveStatistics.getSkewness(),
-                magnetometer.correlation[0], magnetometer.correlation[1], magnetometer.correlation[2],
-                gyroscope.meanVector.getX(), gyroscope.meanVector.getY(), gyroscope.meanVector.getZ(),
-                gyroscope.meanVector.getNorm(), gyroscope.simpleRegression.getSlope(), gyroscope.descriptiveStatistics.getVariance(),
-                gyroscope.descriptiveStatistics.getKurtosis(), gyroscope.descriptiveStatistics.getSkewness(),
-                gyroscope.correlation[0], gyroscope.correlation[1], gyroscope.correlation[2], type);
-
-
-    }
-
-    public double getAccelerationMagnitude() {
-        return acceleration.meanVector.getNorm();
-    }
-
-    static void toCSV(List<AggregatedReading> aggregatedReadings, String filename, String type) {
-        BufferedWriter bufferedWriter;
-        try {
-            String[] params = {"acceleration", "magnetometer", "gyroscope"};
-            String[] HEADERS = buildHeaders(params);
-            bufferedWriter = new BufferedWriter(new FileWriter("aggregated" + filename));
-            CSVPrinter csvPrinter = new CSVPrinter(bufferedWriter, CSVFormat.EXCEL.withHeader(HEADERS));
-            for (AggregatedReading aggregatedReading : aggregatedReadings) {
-                aggregatedReading.printReadingToCSV(csvPrinter, type);
-            }
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public double getAccYZCorrelation() {
@@ -112,12 +148,32 @@ public class AggregatedReading {
         return acceleration.descriptiveStatistics.getVariance();
     }
 
+    public double getAccelerationMagnitude() {
+        return acceleration.meanVector.getNorm();
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Class for all relevant statistics
+     *
+     * Uses the Apache Mathematics Commons library
+     *
+     * @see <a href="http://commons.apache.org/proper/commons-math/"></a>
+     */
     private class Stat {
         private Vector3D meanVector;
         private DescriptiveStatistics descriptiveStatistics;
         private SimpleRegression simpleRegression;
         private double[] correlation;
 
+        /**
+         * Aggregates values until maxTime has been reached; initializes all relevant statistics
+         * @param it an iterator over raw data
+         * @param maxTime the maximum timestamp
+         */
         private Stat(Iterator<InertialSensorRecord> it, long maxTime) {
             simpleRegression = new SimpleRegression(false);
             double[][] filteredData = filterValues(it, maxTime);
@@ -127,21 +183,12 @@ public class AggregatedReading {
             descriptiveStatistics = new DescriptiveStatistics(filteredData[3]);
         }
 
-        private double[] getCorrelation(double[][] data) {
-            double[] corr = {0, 0, 0};
-            try {
-                PearsonsCorrelation pc = new PearsonsCorrelation();
-                corr[0] = pc.correlation(data[0], data[1]);
-                corr[1] = pc.correlation(data[0], data[2]);
-                corr[2] = pc.correlation(data[1], data[2]);
-            } catch (MathIllegalArgumentException e) {
-                corr[0] = 0;
-                corr[1] = 0;
-                corr[2] = 0;
-            }
-            return corr;
-        }
-
+        /**
+         * Gets the values for the parameter in the current time frame
+         * @param it an iterator over raw data
+         * @param maxTime the maximum timestamp
+         * @return a double array containing values (0 for X axis, 1 for Y axis, 2 for Z axis) in the current time frame
+         */
         private double[][] filterValues(Iterator<InertialSensorRecord> it, long maxTime) {
             LinkedList<Vector3D> values = new LinkedList<>();
             InertialSensorRecord next;
@@ -160,6 +207,26 @@ public class AggregatedReading {
                 i++;
             }
             return filteredValues;
+        }
+
+        /**
+         * Calculates XY, XZ and YZ correlations
+         * @param data values for the parameter (0 for x axis, 1 for y axis, 2 for z axis) in the current time frame
+         * @return an array with correlations (0 for XY, 1 for XZ and 2 for  YZ)
+         */
+        private double[] getCorrelation(double[][] data) {
+            double[] corr = {0, 0, 0};
+            try {
+                PearsonsCorrelation pc = new PearsonsCorrelation();
+                corr[0] = pc.correlation(data[0], data[1]);
+                corr[1] = pc.correlation(data[0], data[2]);
+                corr[2] = pc.correlation(data[1], data[2]);
+            } catch (MathIllegalArgumentException e) {
+                corr[0] = 0;
+                corr[1] = 0;
+                corr[2] = 0;
+            }
+            return corr;
         }
     }
 
